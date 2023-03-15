eebd3d8e228f5aa9cb8f4eb7f9828aa151a606d8
Iniobong Benson <linibensonjr@gmail.com>
2023-03-15 23:57:36 +0100

    feat: Register namespace

--- api/__init__.py
+++ api/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_restx import Api
+from .config.config import config_dict
+from .students.views import students_namespace
+from .auth.views import auth_namespace
 
-app = Flask(__name__)
-api = Api(app)
 
-# Path: SchoolifyAPI\api\__init__.py
-from . import api
+def create_app(config=config_dict['dev']):
+    app = Flask(__name__)
+    api = Api(app)
 
-# Path: SchoolifyAPI\api\api.py
-from . import api
+    app.config.from_object(config)
 
-ns = api.namespace('school', description='School API')
+    api.add_namespace(auth_namespace)
+    api.add_namespace(students_namespace)
+ 
+
+    return app
