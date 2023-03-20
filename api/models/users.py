7f78710a1cac2709d29b0921125749725e4038a0
Iniobong Benson <linibensonjr@gmail.com>
2023-03-20 22:38:26 +0100

    chore: update user table

--- api/models/users.py
+++ api/models/users.py
@@ -25,4 +25,47 @@ class Student(db.Model):
         db.session.delete(self)
         db.session.commit()
 
+    @staticmethod
+    def get_all():
+        return Student.query.all()
+    
+    @staticmethod
+    def get_one(student_id):
+        return Student.query.get(student_id)
+    
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    username = db.Column(db.String(128), nullable=False, unique=True)
+    email = db.Column(db.String(128), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    is_active = db.Column(db.Boolean, default=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_modified = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # def __init__(self, username, password):
+    #     self.username = username
+    #     self.password = password
+
+    def __repr__(self):
+        return f"<User {self.id}>"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return User.query.all()
+    
+    @staticmethod
+    def get_one(user_id):
+        return User.query.get(user_id)
+
 
